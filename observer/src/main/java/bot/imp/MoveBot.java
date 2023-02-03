6ba9ee09b1f15c0c16830019bf7b71a870e3cf22
zidane-itb <89019288+zidane-itb@users.noreply.github.com>
2023-02-04 01:42:10 +0700

    Delete observer directory

--- observer/src/main/java/bot/imp/MoveBot.java
+++
@@ -1,9 +0,0 @@
-package bot.imp;
-
-import bot.ActionBot;
-
-public class MoveBot implements ActionBot {
-    public void run(String condition) {
-        System.out.println("move bot is executing.");
-    }
-}
